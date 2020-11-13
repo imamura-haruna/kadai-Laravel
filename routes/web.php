747c4f185bfac66578a9ed1b9576b905868351d9
”imamura-haruna” <became.a.bubble.000@gmail.com>
2020-11-13 05:45:33 +0000

    PHP/Laravel 12 ユーザー認証を実装する の課題

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,17 @@ Route::get('/', function () {
 );*/
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
 
 Route::get('admin/profile/create','Admin\ProfileController@add'
-);
+)->middleware('auth');
 
 Route::get('admin/profile/edit','Admin\ProfileController@edit'
-);
+)->middleware('auth');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
 
