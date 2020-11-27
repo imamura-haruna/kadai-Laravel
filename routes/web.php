029fb9f90abbcb893059659840448339a29310b6
”imamura-haruna” <became.a.bubble.000@gmail.com>
2020-11-27 04:53:16 +0000

    PHP/Laravel 13 ニュース投稿画面を作成しよう

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,11 @@ Route::get('/', function () {
 /*Route::get('http://XXXXXX.jp/XXX','XXX\AAAController@bbb'
 );*/
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+     Route::get('news/create', 'Admin\NewsController@add');
+     Route::post('news/create', 'Admin\NewsController@create'); # 追記
 });
+
 
 Route::get('admin/profile/create','Admin\ProfileController@add'
 )->middleware('auth');
@@ -28,8 +30,11 @@ Route::get('admin/profile/create','Admin\ProfileController@add'
 Route::get('admin/profile/edit','Admin\ProfileController@edit'
 )->middleware('auth');
 
+Route::post('admin/profile/create' , 'Admin\ProfileController@create');
+Route::post('admin/profile/edit','Admin\ProfileController@update');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
 
