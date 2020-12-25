f2ad56b3cdf22fccc13f5b7e72623fe6a8bf140c
”imamura-haruna” <became.a.bubble.000@gmail.com>
2020-12-25 06:36:31 +0000

    Herokuへの画像のアップロード　の授業

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -15,6 +15,9 @@ use App\History;
 // Carbonを使って取得した現在時刻を、Historyモデルの edited_at として記録します。
 use Carbon\Carbon;
 
+//Storageファサードの追加
+use Storage;
+
 class NewsController extends Controller
 {
   public function add()
@@ -34,8 +37,8 @@ class NewsController extends Controller
       
       //フォームから画像が送信されてきたら、保存して、$news->image_path に画像パスを保存する
       if (isset($form['image'])) {
-        $path = $request->file('image')->store('public/image');
-        $news->image_path = basename($path);
+        $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+        $news->image_path = Storage::disk('s3')->url($path);
       } else {
           $news->image_path = null;
       }
@@ -88,8 +91,8 @@ class NewsController extends Controller
       if ($request->remove == 'true') {
           $news_form['image_path'] = null;
       } elseif ($request->file('image')) {
-          $path = $request->file('image')->store('public/image');
-          $news_form['image_path'] = basename($path);
+          $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+          $news->image_path = Storage::disk('s3')->url($path);
       } else {
           $news_form['image_path'] = $news->image_path;
       }
--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -15,7 +15,7 @@
                                     {{-- 画像を表示するコード --}}
                                     {{-- //assetは、「publicディレクトリ」のパスを返すヘルパ（viewファイルで使えるメソッド）となっています。 --}}
                                     {{-- $headline->image_pathで保存した画像のファイル名が入っている。「.」：文字列を結合する。＝画像のパスを返している。 --}}
-                                        <img src="{{ asset('storage/image/' . $headline->image_path) }}">
+                                        <img src="{{ $headline->image_path }}">
                                     @endif
                                 </div>
                                 <div class="title p-2">
@@ -50,7 +50,7 @@
                             </div>
                             <div class="image col-md-6 text-right mt-4">
                                 @if ($post->image_path)
-                                    <img src="{{ asset('storage/image/' . $post->image_path) }}">
+                                    <img src="{{ $post->image_path }}">
                                 @endif
                             </div>
                         </div>
